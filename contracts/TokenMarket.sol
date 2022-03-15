95d2d183f82cd2901144fd5c5a727f1086fd6b10
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-03-15 19:13:15 +0100

    Swap function

--- contracts/TokenMarket.sol
+++ contracts/TokenMarket.sol
@@ -5,11 +5,25 @@ import "./interfaces/IERC20.sol";
 import "./pricefeed/Price.sol";
 
 contract TokenMarket {
+    PriceConverter private p;
+    IERC20 private tokenA;
+    IERC20 private tokenB;
+
     function getPriceExchange(
         address _base,
         address _quote,
         uint8 _decimals
-    ) external returns (init256) {
-        PriceConverter.getDerivedPrice(_base, _quote, _decimals);
+    ) external view returns (int256) {
+        int256 price = p.getDerivedPrice(_base, _quote, _decimals);
+        return price;
+    }
+
+    function swap(
+        address _tokenA,
+        address _tokenB,
+        uint256 decimal
+    ) external returns (bool) {
+        tokenA = IERC20(_tokenA);
+        tokenA = IERC20(_tokenB);
     }
 }
